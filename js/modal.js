9c647ffab2845117fb7c7a7eef3e2de068dd665e
Ogm <watershed@keemail.me>
2021-03-21 17:00:36 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,48 +1,49 @@
 Vue.use(window["vue-js-modal"].default);
+
 var app = new Vue({
-  el: '#app1',
+  el: "#app1",
   methods: {
-    show: function() {
-      this.$modal.show('modal-area1');
+    show: function () {
+      this.$modal.show("modal-1");
     },
-    hide: function() {
-      this.$modal.hide('modal-area1');
+    hide: function () {
+      this.$modal.hide("modal-1");
     },
-  }
+  },
 });
 
 var app = new Vue({
-    el: '#app2',
-    methods: {
-      show: function() {
-        this.$modal.show('modal-area2');
-      },
-      hide: function() {
-        this.$modal.hide('modal-area2');
-      },
-    }
-  });
+  el: "#app2",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-2");
+    },
+    hide: function () {
+      this.$modal.hide("modal-2");
+    },
+  },
+});
 
-  var app = new Vue({
-    el: '#app3',
-    methods: {
-      show: function() {
-        this.$modal.show('modal-area3');
-      },
-      hide: function() {
-        this.$modal.hide('modal-area3');
-      },
-    }
-  });
+var app = new Vue({
+  el: "#app3",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-3");
+    },
+    hide: function () {
+      this.$modal.hide("modal-3");
+    },
+  },
+});
 
-  var app = new Vue({
-    el: '#app4',
-    methods: {
-      show: function() {
-        this.$modal.show('modal-area4');
-      },
-      hide: function() {
-        this.$modal.hide('modal-area4');
-      },
-    }
-  });
+var app = new Vue({
+  el: "#app4",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-4");
+    },
+    hide: function () {
+      this.$modal.hide("modal-4");
+    },
+  },
+});
