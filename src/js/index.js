8084df21219d31bcfd60719458a3f3ffb037bdc3
Ogmer <watershed@keemail.me>
2023-02-22 21:39:36 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -28,51 +28,15 @@ window.addEventListener('DOMContentLoaded', () => {
 Vue.use(window["vue-js-modal"].default);
 
 var app = new Vue({
-  el: "#app1",
+  el: "#app",
   methods: {
-    show: function () {
-      this.$modal.show("modal-1");
+    show: function (modal) {
+      this.$modal.show(modal);
     },
-    hide: function () {
-      this.$modal.hide("modal-1");
+    hide: function (modal) {
+      this.$modal.hide(modal);
     },
   },
-});
-
-var app = new Vue({
-    el: "#app2",
-    methods: {
-      show: function () {
-        this.$modal.show("modal-2");
-      },
-      hide: function () {
-        this.$modal.hide("modal-2");
-      },
-    },
-  });
-
-var app = new Vue({
-    el: "#app3",
-    methods: {
-        show: function () {
-        this.$modal.show("modal-3");
-        },
-        hide: function () {
-        this.$modal.hide("modal-3");
-        },
-    },
-});
-
-var app = new Vue({
-    el: "#app4",
-    methods: {
-        show: function () {
-        this.$modal.show("modal-4");
-        },
-        hide: function () {
-        this.$modal.hide("modal-4");
-        },
-    },
 });
 
 /* 
