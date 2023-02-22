94f8621390809553fc1085e8994304ee96ae24cb
Ogmer <watershed@keemail.me>
2023-02-22 21:48:00 +0900

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -28,18 +28,54 @@ window.addEventListener('DOMContentLoaded', () => {
 Vue.use(window["vue-js-modal"].default);
 
 var app = new Vue({
-  el: "#app",
+  el: "#app1",
   methods: {
-    show: function (modal) {
-      this.$modal.show(modal);
+    show: function () {
+      this.$modal.show("modal-1");
     },
-    hide: function (modal) {
-      this.$modal.hide(modal);
+    hide: function () {
+      this.$modal.hide("modal-1");
     },
   },
 });
 
-/* 
+var app = new Vue({
+    el: "#app2",
+    methods: {
+      show: function () {
+        this.$modal.show("modal-2");
+      },
+      hide: function () {
+        this.$modal.hide("modal-2");
+      },
+    },
+  });
+
+var app = new Vue({
+    el: "#app3",
+    methods: {
+        show: function () {
+        this.$modal.show("modal-3");
+        },
+        hide: function () {
+        this.$modal.hide("modal-3");
+        },
+    },
+});
+
+var app = new Vue({
+    el: "#app4",
+    methods: {
+        show: function () {
+        this.$modal.show("modal-4");
+        },
+        hide: function () {
+        this.$modal.hide("modal-4");
+        },
+    },
+});
+
+/*
 
 $(Window).ready(function(){
   var section = $(".cherry-blossom");
@@ -62,6 +98,6 @@ $(Window).ready(function(){
   });
 
   setInterval(createsakura, 300);
-}); 
+});
 
 */
