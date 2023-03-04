6aa3e61600445db4b1fef81822a531c9d9a69c0b
genki_kumagai <genki.kumagai@gmail.com>
2023-03-05 02:37:28 +0900

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,25 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  const section = document.querySelector('.cherry-blossom');
-
+window.addEventListener("DOMContentLoaded", () => {
+  const section = document.querySelector(".cherry-blossom");
 
   const createsakura = () => {
-    const sakuraEl = document.createElement('span');
-    sakuraEl.className = 'sakura';
+    const sakuraEl = document.createElement("span");
+    sakuraEl.className = "sakura";
     const minSize = 10;
     const maxSize = 15;
     const size = Math.random() * (maxSize + 1 - minSize) + minSize;
     sakuraEl.style.width = `${size}px`;
     sakuraEl.style.height = `${size}px`;
-    sakuraEl.style.left = Math.random() * innerWidth + 'px';
+    sakuraEl.style.left = Math.random() * innerWidth + "px";
     section.appendChild(sakuraEl);
 
     setTimeout(() => {
       sakuraEl.remove();
     }, 10000);
-  }
+  };
 
   setInterval(createsakura, 300);
-
 });
 
 Vue.use(window["vue-js-modal"].default);
@@ -40,39 +37,39 @@ var app = new Vue({
 });
 
 var app = new Vue({
-    el: "#app2",
-    methods: {
-      show: function () {
-        this.$modal.show("modal-2");
-      },
-      hide: function () {
-        this.$modal.hide("modal-2");
-      },
+  el: "#app2",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-2");
     },
-  });
-
-var app = new Vue({
-    el: "#app3",
-    methods: {
-        show: function () {
-        this.$modal.show("modal-3");
-        },
-        hide: function () {
-        this.$modal.hide("modal-3");
-        },
+    hide: function () {
+      this.$modal.hide("modal-2");
     },
+  },
 });
 
 var app = new Vue({
-    el: "#app4",
-    methods: {
-        show: function () {
-        this.$modal.show("modal-4");
-        },
-        hide: function () {
-        this.$modal.hide("modal-4");
-        },
+  el: "#app3",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-3");
     },
+    hide: function () {
+      this.$modal.hide("modal-3");
+    },
+  },
+});
+
+var app = new Vue({
+  el: "#app4",
+  methods: {
+    show: function () {
+      this.$modal.show("modal-4");
+    },
+    hide: function () {
+      this.$modal.hide("modal-4");
+    },
+  },
 });
 
 /*
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   mode: "production",
 
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
 
   optimization: {
-    minimizer: [
-      new TerserPlugin({}),
-      new CssMinimizerPlugin(),
-    ]
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin()],
   },
 
   module: {
@@ -40,9 +37,7 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         options: {
-          presets: [
-            "@babel/preset-env",
-          ],
+          presets: ["@babel/preset-env"],
         },
       },
     ],
@@ -55,12 +50,12 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: "style.css",
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: "./src/index.html",
     }),
-    new VueLoaderPlugin() 
+    new VueLoaderPlugin(),
   ],
   target: ["web", "es5"],
 };
