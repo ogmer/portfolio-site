dd45044ddf30e6f2bbc307f787cf858fa872581f
genki_kumagai <genki.kumagai@gmail.com>
2023-07-09 04:03:09 +0900

    add space

--- src/js/index.js
+++ src/js/index.js
@@ -27,10 +27,10 @@ Vue.use(window["vue-js-modal"].default);
 var app = new Vue({
   el: "#app1",
   methods: {
-    show: function () {
+    show: function() {
       this.$modal.show("modal-1");
     },
-    hide: function () {
+    hide: function() {
       this.$modal.hide("modal-1");
     },
   },
@@ -39,10 +39,10 @@ var app = new Vue({
 var app = new Vue({
   el: "#app2",
   methods: {
-    show: function () {
+    show: function() {
       this.$modal.show("modal-2");
     },
-    hide: function () {
+    hide: function() {
       this.$modal.hide("modal-2");
     },
   },
@@ -51,10 +51,10 @@ var app = new Vue({
 var app = new Vue({
   el: "#app3",
   methods: {
-    show: function () {
+    show: function() {
       this.$modal.show("modal-3");
     },
-    hide: function () {
+    hide: function() {
       this.$modal.hide("modal-3");
     },
   },
@@ -63,10 +63,10 @@ var app = new Vue({
 var app = new Vue({
   el: "#app4",
   methods: {
-    show: function () {
+    show: function() {
       this.$modal.show("modal-4");
     },
-    hide: function () {
+    hide: function() {
       this.$modal.hide("modal-4");
     },
   },
