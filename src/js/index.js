5738e7a0c48f284b0c0461510f8e06deaba62ab5
ogmer <genki.kumagai@gmail.com>
2024-12-06 05:14:05 +0900

    delete jQuery

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,23 @@
-"use strict";
-
-// Constants
 const SAKURA_CONFIG = {
   minSize: 10,
   maxSize: 15,
   animationDuration: 10000,
   createInterval: 300,
 };
-
-// Cherry blossom effect
-$(document).ready(() => {
-  const $section = $(".cherry-blossom");
-
-  const createSakura = () => {
-    const size =
-      Math.random() * (SAKURA_CONFIG.maxSize - SAKURA_CONFIG.minSize) +
-      SAKURA_CONFIG.minSize;
-    const positionX = Math.random() * $(window).width();
-    const positionY = Math.random() * $(window).height();
-
-    const sakuraStyle = {
-      width: `${size}px`,
-      height: `${size}px`,
-      left: `${positionX}px`,
-      top: `${positionY}px`,
-      animationDuration: `${SAKURA_CONFIG.animationDuration}ms`,
-    };
-
-    const $sakuraEl = $('<span class="sakura"></span>').css(sakuraStyle);
-    $section.append($sakuraEl);
-
-    $sakuraEl.on("animationend", () => {
-      $sakuraEl.remove();
-    });
-  };
-
-  setInterval(createSakura, SAKURA_CONFIG.createInterval);
-});
-
-// Modal management
 new Vue({
-  el: ".menu",
+  el: ".cherry-blossom",
   data: {
     showModal1: false,
     showModal2: false,
     showModal3: false,
     showModal4: false,
+    sakuraConfig: {
+      minSize: 10,
+      maxSize: 15,
+      animationDuration: 10000,
+      createInterval: 300,
+    },
+    sakuraElements: [],
   },
   methods: {
     showModal(modalId) {
@@ -58,5 +30,39 @@ new Vue({
       this['showModal' + modalNumber] = false;
       console.log(`Closed modal${modalNumber}`);
     },
+    createSakura() {
+      const size =
+        Math.random() * (this.sakuraConfig.maxSize - this.sakuraConfig.minSize) +
+        this.sakuraConfig.minSize;
+      const positionX = Math.random() * window.innerWidth;
+      const positionY = Math.random() * window.innerHeight;
+
+      const sakuraStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+        left: `${positionX}px`,
+        top: `${positionY}px`,
+        animationDuration: `${this.sakuraConfig.animationDuration}ms`,
+      };
+
+      const sakura = { id: Date.now(), style: sakuraStyle };
+      this.sakuraElements.push(sakura);
+
+      setTimeout(() => {
+        this.sakuraElements = this.sakuraElements.filter(el => el.id !== sakura.id);
+      }, this.sakuraConfig.animationDuration);
+    },
+    startSakura() {
+      this.sakuraInterval = setInterval(this.createSakura, this.sakuraConfig.createInterval);
+    },
+    stopSakura() {
+      clearInterval(this.sakuraInterval);
+    },
+  },
+  mounted() {
+    this.startSakura();
+  },
+  beforeDestroy() {
+    this.stopSakura();
   },
 });
