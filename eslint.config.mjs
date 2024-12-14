561a9c9e73b21fd47013842ef9906b172e47effa
ogmer <genki.kumagai@gmail.com>
2024-12-14 11:41:14 +0900

    fix code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,13 @@ import pluginVue from "eslint-plugin-vue";
 export default [
   { files: ["**/*.{js,mjs,cjs,vue}"] },
   pluginJs.configs.recommended,
-  ...pluginVue.configs["essential"], // "flat/essential" を "essential" に変更
+  ...pluginVue.configs["essential"],
   {
     rules: {
-      "no-console": "warn", // コンソール使用を警告に設定
-      "vue/no-unused-vars": "error", // 未使用変数をエラーに設定
-      "no-unused-expressions": "error", // 未使用の式をエラーに設定
-      "prefer-const": "error" // 追加ルール
+      "no-console": "warn",
+      "vue/no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error"
     },
   },
 ];
